a0eb63cef2beb476af53b1017b3b1cbe7315c336
LeeYuan <LeeYuuuan@outlook.com>
2023-09-08 17:03:36 +0800

    a

---
+++ ref.md
@@ -0,0 +1,9 @@
+- [residual block and attention block](https://zhuanlan.zhihu.com/p/563661713)
+- [A convnet for 2020s](https://zhuanlan.zhihu.com/p/490816354)
+- [U-net in stable diffusion](https://zhuanlan.zhihu.com/p/635204519)
+- [U-net](https://zhuanlan.zhihu.com/p/43927696)
+- [others](https://zhuanlan.zhihu.com/p/567780741)
+- [Group Norm](https://arxiv.org/abs/1803.08494)
+## Notes:
+
+- 
--- unet_parts.py
+++ unet_parts.py
@@ -3,7 +3,47 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+from inspect import isfunction
+from functools import partial
+import math
 
+def exists(x):
+    return x is not None
+
+def default(val, d):
+    if exists(val):
+        return val
+    return d() if isfunction(d) else d
+
+class Residual(nn.Module):
+    def __init__(self, fn):
+        super().__init__()
+        self.fn = fn
+    
+    def forward(self, x, *args, **kwargs):
+        return self.fn(x, *args, **kwargs) + x
+    
+def Upsample(dim):
+    return nn.ConvTranspose2d(dim, dim, 4, 2, 1)
+
+def Downsample(dim):
+    return nn.Conv2d(dim, dim, 4, 2, 1)
+
+class SinusoidalPositionEmbeddings(nn.Module):
+    def __init__(self, dim):
+        super().__init__()
+        self.dim = dim
+    
+    def forward(self, time):
+        device = time.device
+        half_dim = self.dim // 2 
+        embeddings  = math.log(10000) / (half_dim - 1)
+        embeddings = torch.exp(torch.arrange(half_dim, device=device) * -embeddings)
+        embeddings = time[:, None] * embeddings[None, :]
+        embeddings = torch.cat((embeddings.sin(), embeddings.cos()), dim=-1)
+        return embeddings
+    
+    
 class Conv_part(nn.Module):
     
     def __init__(self, in_channels, out_channels, mid_channels=None) -> None:
